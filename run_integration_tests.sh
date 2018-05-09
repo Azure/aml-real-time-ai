386854783a361adff6a9d97f601a1df21e04d18e
Daniel Hartl <danhartl@microsoft.com>
2018-05-09 14:05:20 -0700

    Fix script

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo $1
+conda env create -f environment.yml
+source /etc/conda/bin/activate amlrealtimeai
+pip install pytest
+pip install -e $(dirname "$0")/pythonlib --user
+export TEST_SERVICE_PRINCIPAL_KEY=$1
+pytest pythonlib/tests/integration_tests
+ERR=$?
+source /etc/conda/bin/activate base
+conda env remove -y -n amlrealtimeai
+exit $ERR
