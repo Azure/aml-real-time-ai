14a5449d32b546e710e2d33189230b785c81865d
Daniel Hartl <danhartl@microsoft.com>
2018-05-09 13:57:58 -0700

    Arg test

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-conda env create -f environment.yml
-source /etc/conda/bin/activate amlrealtimeai
-pip install pytest
-pip install -e $(dirname "$0")/pythonlib --user
-pytest pythonlib/tests/integration_tests
-ERR=$?
-source /etc/conda/bin/activate base
-conda env remove -y -n amlrealtimeai
-exit $ERR
+echo $1
