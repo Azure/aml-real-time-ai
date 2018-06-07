1a7ea3b2fdfa09dbdbb2b0ec272e1385c9ca6276
Daniel Hartl <danhartl@microsoft.com>
2018-06-07 12:30:08 -0700

    Update test script

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+dotnet test --project sample-clients/csharp/client.tests
+ERR=$?
+
+if [ $? -ne 0 ]
+then
+    exit $ERR
+fi
+
+
 conda env create -f environment.yml
-source /etc/conda/bin/activate amlrealtimeai
+source /etc/conda/bin/activate azureml_fpga
 pip install pytest
 pip install -e $(dirname "$0")/pythonlib --user
 pytest pythonlib/tests/unit_tests
 ERR=$?
 source /etc/conda/bin/activate base
-conda env remove -y -n amlrealtimeai
-exit $ERR
+conda env remove -y -n azureml_fpga
+if [ $ERR -ne 0 ]
+then
+    exit $ERR
+fi
+
+
