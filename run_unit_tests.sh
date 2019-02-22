5f03bfb403faab264274a9a72359957a6f588240
coverste <coverste@microsoft.com>
2019-02-22 15:46:16 -0800

    try with conda

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -8,10 +8,10 @@ then
 fi
 
 
-sudo conda env create -f environment.yml
+conda env create -f environment.yml
 conda activate amlrealtimeai
-sudo conda install -y pytest pytest-cov
-pytest --cov=pythonlib/amlrealtimeai pythonlib/tests/unit_tests
+conda install -y pytest pytest-cov
+python -m pytest --cov=pythonlib/amlrealtimeai pythonlib/tests/unit_tests
 ERR=$?
 conda activate base
 conda env remove -y -n amlrealtimeai
