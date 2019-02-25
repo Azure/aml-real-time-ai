3c3a7055eca8d69db08eddfd4f3eb52a8a46566c
Colin Versteeg <cverstee@gmail.com>
2019-02-25 10:39:07 -0800

    enable azure pipelines (#36)
    * enable azure pipelines
    
    * changes for hosted nodes
    
    * update build script

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -6,14 +6,14 @@ if [ $ERR -ne 0 ]
 then
     exit $ERR
 fi
-
+source /usr/share/miniconda/etc/profile.d/conda.sh
 
 conda env create -f environment.yml
-source /etc/conda/bin/activate amlrealtimeai
+conda activate amlrealtimeai
 conda install -y pytest pytest-cov
-pytest --cov=pythonlib/amlrealtimeai pythonlib/tests/unit_tests
+python -m pytest --cov=pythonlib/amlrealtimeai pythonlib/tests/unit_tests
 ERR=$?
-source /etc/conda/bin/activate base
+conda activate base
 conda env remove -y -n amlrealtimeai
 if [ $ERR -ne 0 ]
 then
